92e370bb1c5378230414e6a75d949d2f4991d0b3
ERICK <erick_honorio20@hotmail.com>
2022-06-13 14:54:45 -0300

    dist

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/projeto16/'
-    : '/'
-}
 
 module.exports = {
   css: {
@@ -16,7 +11,4 @@ module.exports = {
       }
     }
   }
-};
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/projeto15/" : "/",
 };
